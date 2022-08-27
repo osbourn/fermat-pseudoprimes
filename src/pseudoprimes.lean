b4aab5233c0e0324eba2f02f039385e982fbe8f9
Niels Voss <code@nielsvoss.com>
2022-08-27 10:52:33 -0700

    Start working on next_in_sequence theorem

--- src/pseudoprimes.lean
+++ src/pseudoprimes.lean
@@ -8,27 +8,56 @@ instance decidable_probable_prime (n : ℕ) (b : ℕ) : decidable (probable_prim
 nat.decidable_dvd _ _
 
 definition pseudoprime (n : ℕ) (b : ℕ) : Prop :=
-nat.coprime n b ∧ probable_prime n b ∧ ¬nat.prime n
+nat.coprime n b ∧ probable_prime n b ∧ ¬nat.prime n ∧ n > 1
 
 instance decidable_pseudoprime (n : ℕ) (b : ℕ) :
   decidable (pseudoprime n b) := and.decidable
 
-lemma pseudoprime_of_base_one (n : ℕ) (not_prime : ¬nat.prime n) : pseudoprime n 1 :=
+lemma pseudoprime_of_base_one (n : ℕ) (n_gt_one : n > 1) (not_prime : ¬nat.prime n) : pseudoprime n 1 :=
 begin
   split,
   { norm_num },
   { split, 
     { have h : 0 = 1^(n - 1) - 1 := by norm_num,
       show n ∣ 1^(n - 1) - 1, from h ▸ (dvd_zero n) },
-    { exact not_prime } }
+    { exact ⟨not_prime, n_gt_one⟩ } }
 end
 
-private lemma div_lem (m n b : ℕ) (b_ne_one : b ≠ 1) (m_dvd_n : m ∣ n) : (b^m - 1) ∣ (b^n - 1) :=
+private lemma dvd_lem (m n b : ℕ) (b_ne_one : b ≠ 1) (m_dvd_n : m ∣ n) : (b^m - 1) ∣ (b^n - 1) :=
 sorry
 
-private lemma div_lem2 (m n : ℕ) : m ∣ n → (2^m - 1) ∣ (2^n - 1) :=
-div_lem m n 2 dec_trivial
+private lemma dvd_lem2 (m n : ℕ) : m ∣ n → (2^m - 1) ∣ (2^n - 1) :=
+dvd_lem m n 2 dec_trivial
 
+private theorem next_in_sequence (n : ℕ) (n_psp : pseudoprime n 2) : pseudoprime (2^(n - 1) - 1) 2 :=
+begin
+  generalize h : 2^n - 1 = m,
+  have m_gt_one : m > 1 := sorry,
+  have m_coprime_two : nat.coprime m 2 := sorry,
+  have m_not_prime : ¬nat.prime m := begin
+    rw ← h,
+    have n_not_prime := and.elim_left (and.elim_right (and.elim_right n_psp)),
+    have min_fac_n_dvd : n.min_fac ∣ n := nat.min_fac_dvd n,
+    have min_fac_n_gt_two : n.min_fac > 2 := sorry,
+    have dvd_lem_res := dvd_lem n.min_fac n 2 dec_trivial min_fac_n_dvd,
+    have fac_of_m_lt_m : 2 ^ n.min_fac - 1 < 2 ^ n - 1 := sorry,
+    have fac_of_m_gt_one : 2 ^ n.min_fac - 1 > 1 := sorry,
+    intro h₁,
+    have h₂ := nat.prime.eq_one_or_self_of_dvd h₁ (2^(n.min_fac) - 1) dvd_lem_res,
+    cases h₂,
+    { exact (ne_of_gt fac_of_m_gt_one) h₂ },
+    { exact (ne_of_lt fac_of_m_lt_m) h₂ }
+  end,
+  have n_dvd : n ∣ (m - 1) := sorry,
+  have k : ℕ := (m - 1) / n,
+  have k_mul_n : k*n = m - 1 := sorry,
+  have m_div_two_pow_kn : m ∣ (2^(k*n) - 1) := sorry,
+  have m_div_two_pow_m_sub_one : m ∣ (2^(m - 1) - 1) := by rwa ← k_mul_n,
+  have probable_prime_m : probable_prime m 2 := m_div_two_pow_m_sub_one,
+  exact ⟨m_coprime_two, probable_prime_m, m_not_prime, m_gt_one⟩
+end
+
+/--/
 theorem infinite_pseudoprimes_base_two : ∀ (a : ℕ), ∃ (p : ℕ), p ≥ a ∧ pseudoprime p 2 :=
 begin
   intro a,
@@ -58,9 +87,8 @@ begin
   have m_psp : pseudoprime m 2 := ⟨m_coprime_two, m_dvd, m_not_prime⟩,
   sorry
 end
-
-#eval if (pseudoprime 341 2) then "yes" else "no"
+-/
 
 end fermat_pseudoprimes
 
-#check @eq.subst
+#check nat.prime.eq_one_or_self_of_dvd
