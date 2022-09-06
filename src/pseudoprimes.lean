758b8369be6c08debe9e32d22f162179df695ca8
Niels Voss <programming@nielsvoss.com>
2022-09-06 00:11:18 -0700

    Prove mul_self

--- src/pseudoprimes.lean
+++ src/pseudoprimes.lean
@@ -36,7 +36,9 @@ calc (a + b) * (a - b) = a*(a - b) + b*(a - b) : by rw add_mul
                    ... = a*a - a*b + a*b - b*b : by rw mul_comm b a
                    ... = a*a - b*b : by rw nat.sub_add_cancel h₁
 lemma not_dvd_of_not_dvd_mul (a b c : ℕ) (h : ¬a ∣ b * c) : ¬a ∣ b := λ h₁, h (dvd_mul_of_dvd_left h₁ c)
-lemma mul_self (n : ℕ) : n * n = n ^ 2 := sorry
+lemma mul_self (n : ℕ) : n * n = n ^ 2 := calc n * n = n * n^1 : by rw pow_one
+                                                 ... = n^2 : rfl
+#exit
 lemma pow_factor (a b : ℕ) (h : b ≥ 1) : a^b = a * a^(b - 1) := begin
   have : b - 1 + 1 = b := by rw nat.sub_add_cancel h,
   rw ←this,
